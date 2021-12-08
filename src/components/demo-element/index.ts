cffe93f80f0d27c74ac7a84c8369e71cf098f5bb
danieldost <dost.daniel@icloud.com>
2021-12-08 19:15:18 +0300

    added desc prop for demo el

--- src/components/demo-element/index.ts
+++ src/components/demo-element/index.ts
@@ -8,6 +8,8 @@ import logo from '../../../assets/open-wc-logo.svg';
 export class DemoElement extends LitElement {
   @property({ type: String }) title = 'My app';
 
+  @property({ type: String }) description = 'Some description text';
+
   static styles = styles;
 
   render() {
@@ -15,6 +17,7 @@ export class DemoElement extends LitElement {
       <main>
         <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
         <h1>${this.title}</h1>
+        <p>${this.description}</p>
         <p>Edit <code>src/DemoElement.ts</code> and save to reload.</p>
         <a
           class="app-link"
